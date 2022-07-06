bc48fe0acf97e148405b42ee8b682756c5c53d51
Sergey Rukhlov <rukhlov-s@mail.ru>
2022-07-06 22:00:26 +0300

    Ruineed1

---
+++ Ruineed1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл,
+//который принимает на вход два числа (A и B) и возводит
+//число A в натуральную степень B.
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine();
+
+        int number = 3;
+        int exponent = 5;
+        int count = 1;
+        int result = number;
+
+        while (count < exponent)
+        {
+            result = result * number;
+            count++;
+        }
+        Console.WriteLine($"{number} ^ {exponent} = {result} ");
+        Console.WriteLine();
+    }
+}
