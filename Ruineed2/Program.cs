1f5b562e867432ea7da8939f764f1699cb9931c6
Sergey Rukhlov <rukhlov-s@mail.ru>
2022-07-06 23:00:58 +0300

    Ruineed2

---
+++ Ruineed2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает 
+//на вход число и выдаёт сумму цифр в числе.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        System.Console.WriteLine();
+
+        System.Console.Write("Введите число:   ");
+        string writeNum = Console.ReadLine();
+        int number = Convert.ToInt32(writeNum);
+        int result = number;
+
+        int count1 = 0;
+        while (result > 0)
+        {
+            result = result / 10;
+            count1++;
+        }
+        System.Console.WriteLine($"В числе {number} {count1} цифр");
+        System.Console.WriteLine();
+    }
+}
